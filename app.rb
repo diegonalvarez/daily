35320f4c0769b8a3b4aa80724455b43f69b2de30
Diego Alvarez <diegoalvarez17@gmail.com>
2016-04-25 00:07:51 -0300

    update variables

--- app.rb
+++ app.rb
@@ -14,16 +14,17 @@ class App < Sinatra::Base
   # Load core configuration file
   config_file 'config/app_config.yml'
 
-  # use Rack::Auth::Basic, "Protected Area" do |username, password|
-  #   username == settings.username && password == settings.password
-  # end
+  use Rack::Auth::Basic, "Protected Area" do |username, password|
+    username == settings.username && password == settings.password
+  end
 
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/#{settings.database}.db")
 
-  #run! if app_file == $0
+  run! if app_file == $0
 end
 
 require_relative 'models/init'
 require_relative 'modules/init'
 require_relative 'routes/init'
+DataMapper.auto_upgrade!
 DataMapper.finalize
