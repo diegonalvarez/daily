163444f49360bb4ac5f55a4a3f8b479207a31914
Diego Alvarez <diegoalvarez17@gmail.com>
2016-04-25 22:14:38 -0400

    added new version git

--- modules/daily.rb
+++ modules/daily.rb
@@ -3,6 +3,6 @@ require 'hipchat'
 class Daily
   def send(app_key, text)
     client = HipChat::Client.new(app_key, :api_version => 'v2')
-    client['Abogex'].send('App Daily', text, :color => 'purple', :message_format => 'text', :notify => true)
+    client['Pcx Devs'].send('App Daily', text, :color => 'purple', :message_format => 'text', :notify => true)
   end
 end
